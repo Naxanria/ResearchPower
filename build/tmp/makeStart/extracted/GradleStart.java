93ffc74fc3bdf1393a1bf61ed19a4e801b3ba55f
Aaron Mills <cloudhunter@btinternet.com>
2017-11-22 17:53:14 +0000

    Attempt two at .gitignore update

--- build/tmp/makeStart/extracted/GradleStart.java
+++ build/tmp/makeStart/extracted/GradleStart.java
@@ -43,7 +43,7 @@ public class GradleStart extends GradleStartCommon
     {
         argMap.put("version",        "1.12");
         argMap.put("assetIndex",     "1.12");
-        argMap.put("assetsDir",      "C:/Users/Naxanria/.gradle/caches/minecraft/assets");
+        argMap.put("assetsDir",      "C:/Users/aaron/.gradle/caches/minecraft/assets");
         argMap.put("accessToken",    "FML");
         argMap.put("userProperties", "{}");
         argMap.put("username",        null);
@@ -68,7 +68,7 @@ public class GradleStart extends GradleStartCommon
     private static void hackNatives()
     {
         String paths = System.getProperty("java.library.path");
-        String nativesDir = "C:/Users/Naxanria/.gradle/caches/minecraft/net/minecraft/natives/1.12";
+        String nativesDir = "C:/Users/aaron/.gradle/caches/minecraft/net/minecraft/natives/1.12";
         
         if (Strings.isNullOrEmpty(paths))
             paths = nativesDir;
